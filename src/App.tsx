d3aa29b3156ea8764bff341b95004b6b6b0c84c8
aleksandr <chy4egg@gmail.com>
2020-03-08 18:07:32 +0300

    added search params for page changing

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {IInitialState} from "src/store/state";
 import {IArticle} from "src/models/articles";
 
 const pageSize = 6;
+const articlesStorageKey = 'react-app__local-articles';
 
 const emptyFormData = {
   img: '',
@@ -22,23 +23,71 @@ const emptyFormData = {
 const App: React.FC = () => {
   const dispatch = useDispatch();
   const articles = useSelector<IInitialState, any>((state: IInitialState) => state.articles);
+  const [isMounted, setIsMounted] = useState(false);
   const [formData, setFormData] = useState<IArticle>(emptyFormData);
   const [filteredList, setFilteredList] = useState(articles);
-  const [isModalOpen, setIsModalOpen] = useState(true); // TODO FALSE !
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [page, setPage] = useState(1);
   const [count, setCount] = useState(1);
+
+  const updatePage = (page: number) => {
+    setPage(page);
+    const params = new URLSearchParams(window.location.search);
+    params.set('page', page.toString());
+    const newUrl = window.location.protocol + '//' + window.location.host + window.location.pathname + '?' + params.toString();
+    window.history.pushState({path: newUrl}, '', newUrl);
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get('page');
+    if (page) {
+      setPage(+page);
+    }
+  }, []);
+
+  useEffect(() => {
+    getArticlesFromLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      saveArticlesInLocalStorage();
+    } else {
+      setIsMounted(true);
+    }
+  }, [articles]);
 
   useEffect(() => {
     setCount(Math.ceil(articles.length / pageSize))
   }, [articles.length]);
 
   const handlePageChange = (page: number) => {
-    setPage(page);
+    updatePage(page);
   };
 
   useEffect(() => {
     setFilteredList(paginator(articles, pageSize, page))
   }, [page, articles]);
+
+  // fixme: не обновляет из localStorage
+  const getArticlesFromLocalStorage = () => {
+    console.log('get articles!');
+    const local = window.localStorage.getItem(articlesStorageKey);
+    if (local) {
+      const parsedLocal = JSON.parse(local) as { articles: IArticle[] };
+      if (parsedLocal.articles) {
+        // TODO: обновлять через REDUX
+        // parsedLocal.articles
+        // addArticle(formData)(dispatch);
+      }
+    }
+  };
+
+  const saveArticlesInLocalStorage = () => {
+    const objectToSave = { articles };
+    window.localStorage.setItem(articlesStorageKey, JSON.stringify(objectToSave));
+  };
 
   const handleAddArticle = () => {
     setIsModalOpen(true);
